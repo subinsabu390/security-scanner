ce56628b27d299904238e39f39f3d451d8917b0e
Subin Sabu <subinsabu390@gmail.com>
2025-07-17 16:29:23 +0530

    Added scripts for kubescape, popeye and trivy

--- popeye-script.sh
+++ popeye-script.sh
@@ -5,4 +5,3 @@ POPEYE_REPORT_DIR=$(pwd) popeye --save --out html --output-file popeye-report.ht
 az storage blob upload --account-name $STORAGE_ACCOUNT --container-name $CONTAINER_NAME \
   --file "popeye-report.html" --name "security-scan/$(date +'%d-%m-%Y')/popeye-report.html" \
   --account-key "$AZURE_STORAGE_KEY" --overwrite
-
